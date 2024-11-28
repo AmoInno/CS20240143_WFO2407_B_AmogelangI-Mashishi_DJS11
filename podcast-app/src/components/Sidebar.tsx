59c1d7e5352f098f7bb2351844e6d57dffa35f80
AmoInno <amogelangmashishi12@gmail.com>
2024-11-28 14:23:18 +0200

    install react & tailwindcss

---
+++ podcast-app/src/components/Sidebar.tsx
@@ -0,0 +1,32 @@
+function Sidebar() {
+  return (
+    <>
+      <div>
+        <div>
+          <img src="./src/assets/home.png" alt="Home icon" />
+          <p>Home</p>
+        </div>
+        <div>
+          <img src="./src/assets/search.png" alt="Search icon" />
+          <p>Search</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <img src="./src/assets/library.png" alt="Library icon" />
+          <p>My Library</p>
+        </div>
+        <div>
+          <img src="./src/assets/favourites.png" alt="Favourite icon" />
+          <p>Favourites</p>
+        </div>
+        <div>
+          <img src="./src/assets/subscription.jpg" alt="Subscriptions icon" />
+          <p>Subscriptions</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
